ee3b603e48bb45fa0d0f9479541c07eebd5e2ba3
Pip <92968130+codepip55@users.noreply.github.com>
2025-03-07 20:07:26 +0100

    Make info page work

--- src/app/pages/dashboard/service-planner/info/info.component.ts
+++ src/app/pages/dashboard/service-planner/info/info.component.ts
@@ -1,21 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCircleCheck, faCircleQuestion, faCircleXmark, faFloppyDisk } from '@fortawesome/free-regular-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { AlertService } from '../../../../services/alert.service';
 
 @Component({
   selector: 'app-info',
   imports: [
-    FaIconComponent
+    FaIconComponent,
+    ReactiveFormsModule,
+    NgFor
   ],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit{
+
+  constructor(private alertService: AlertService) {
+  }
+
+  @ViewChild('positionName') positionName!: HTMLInputElement;
+  @ViewChild('teamName') teamName!: HTMLInputElement;
 
   protected readonly faCircleQuestion = faCircleQuestion;
   protected readonly faCircleCheck = faCircleCheck;
   protected readonly faCircleXmark = faCircleXmark;
   protected readonly faTrash = faTrash;
   protected readonly faFloppyDisk = faFloppyDisk;
+
+  serviceForm!: FormGroup;
+
+  ngOnInit() {
+    this.serviceForm = new FormGroup({
+      title: new FormControl('Zondagochtend', Validators.required),
+      date: new FormControl('', Validators.required),
+      startTime: new FormControl('', Validators.required),
+      endTime: new FormControl('', Validators.required),
+      location: new FormControl('Reggestroom (Reggestraat 100, Enter)', Validators.required),
+      manager: new FormControl('', Validators.required),
+      teams: new FormArray([])
+    })
+  }
+  get teams () {
+    return this.serviceForm.get('teams') as FormArray;
+  }
+  addTeam(teamName: string) {
+    // Check if team already exists
+    for (let i = 0; i < this.teams.length; i++) {
+      if (this.teams.at(i).get('team')?.value === teamName) {
+        this.alertService.add({ type: 'warning', message: 'Dit team bestaat al.' });
+        return;
+      }
+    }
+
+    const teamGroup = new FormGroup({
+      team: new FormControl(teamName, Validators.required),
+      positions: new FormArray([])
+    });
+    this.teams.push(teamGroup);
+    this.teamName.value = '';
+  }
+  addPosition(index: number, positionName: string) {
+    const positions = this.teams.at(index).get('positions') as FormArray;
+
+    for (let i = 0; i < positions.length; i++) {
+      if (positions.at(i).value === positionName) {
+        this.alertService.add({ type: 'warning', message: 'Deze positie bestaat al in dit team.' });
+        return;
+      }
+    }
+
+    positions.push(new FormControl(positionName, Validators.required));
+  }
+  onSubmit() {
+    if (this.serviceForm.valid) {
+      console.log(this.serviceForm.value);
+    }
+  }
 }
